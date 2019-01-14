6b6c8b55e0f8a742e4ee40b243f74282de49cbd6
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-14 16:48:54 +0000

    Add test for default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,21 @@
 require 'oystercard'
 
-describe Oystercard do
+describe OysterCard do
+
+  before(:each) do
+    @card = OysterCard.new
+  end
+
+  describe 'setting up the OysterCard' do
+
+    it 'should have an initial balance equal to default balance' do
+      expect(@card.balance).to eq(OysterCard::DEFAULT_BALANCE)
+    end
+
+    it 'should have a default balance equal to 0' do
+      expect(OysterCard::DEFAULT_BALANCE).to eq(0)
+    end
+
+  end
 
 end
