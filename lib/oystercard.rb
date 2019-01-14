1e9f346ccf6ca9505a27ce3d1b0bb0129aa8fb20
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-14 17:34:13 +0000

    Implement consumer protection tests with maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,8 @@
 class OysterCard
 
   DEFAULT_BALANCE = 0
+  MAXIMUM_BALANCE = 90
+  MAX_BALANCE_ERROR = "Card limit of #{MAXIMUM_BALANCE} reached".freeze
 
   attr_reader :balance
 
@@ -9,7 +11,9 @@ class OysterCard
   end
 
   def top_up(amount)
-    @balance += amount 
+    raise MAX_BALANCE_ERROR if amount + balance >= MAXIMUM_BALANCE
+
+    @balance += amount
   end
 
 end
